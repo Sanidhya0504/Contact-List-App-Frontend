10c80cb7c28a50c3710be86a87b1a68fdf0cf0ee
Sanidhya <sharmasanidhya0504@gmail.com>
2023-06-04 13:59:58 +0530

    made responsive

--- src/Home/home.js
+++ src/Home/home.js
@@ -12,16 +12,18 @@ function Home() {
       <div className=" d-flex flex-column justify-content-center align-items-center text-center background">
         <div className="shadow-lg p-3 mb-5 bg-white rounded d-flex justify-content-center align-items-center text-center home-wrap">
           <div className="text-wrap w-75">
-            <h1 className="display-5">Contact List Full Stack Application</h1>
+            <h1 className="display-5 text1">
+              Contact List Full Stack Application
+            </h1>
             <br />
-            <p className="lead">
+            <p className="lead text2">
               This is a Contact List Full Stack Application which allows users
               to add, edit and delete contact details. This application is
               intended for:
             </p>
-            <p className="display-6">Xeno Assignment- SDE Internship.</p>
+            <p className="display-6 text3">Xeno Assignment- SDE Internship.</p>
             <hr className="my-4" />
-            <p>
+            <p className="text4">
               This application is built using ReactJS, NodeJS, ExpressJS and
               MongoDb. It uses Json Web Tokens for authentication and
               authorization.
